a406a7dc5c471604a8c7d31111f0ed0c2bc4b437
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-02-07 19:03:06 +0300

    Delete js

--- js/newtab.js
+++
@@ -1,4 +0,0 @@
-function toggleText(id) {
-        var button = document.getElementById(id).firstChild;
-        button.innerText = "pressed"
-        button.data = button.data == "Lock" ? "Unlock" : "Lock";
