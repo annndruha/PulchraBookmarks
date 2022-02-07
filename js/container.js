0295a0a95fc3c62fb694ed7e1b3055f54d951955
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-02-07 19:43:19 +0300

    Add grid

---
+++ js/container.js
@@ -0,0 +1,12 @@
+function makeRows(rows, cols) {
+    const container = document.getElementById("container");
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    //cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(3, 5);
