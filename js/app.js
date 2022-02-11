a4a6d56ca1d716ca1d3b168242b64bb2ac7ab2f5
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-02-11 14:32:15 +0300

    Fix behavior

--- js/app.js
+++ js/app.js
@@ -3,13 +3,15 @@ let cols = 5
 let rows = 3
 makeGrid(container, cols, rows);
 
+debug = true
 
 $(document).ready(function(){
     $('.grid-item-inside').click(function () {
         let link = this.getAttribute("link");
-        if (link !== "undefined" && link !== "null") {
-            chrome.tabs.update({active: true, url: link});
+        if (link !== "undefined" && link !== "null" && link !== "") {
             //chrome.tabs.create({"url": "https://" + link});
+            chrome.tabs.update({active: true, url: link});
+            console.log(typeof link + " : " + link)
         }
         else {
             alert("Empty link")
