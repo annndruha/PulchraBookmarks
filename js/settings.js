aa3de7b4ec144f5490d570f300c6649f29bb24da
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-02-14 06:24:36 +0300

    Update settings

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,22 @@
 function openSettings(){
     $(".settings").css("right", 0)
+    $.getJSON("manifest.json", function(json) {
+        console.log(json["version"]);
+        let ver = document.getElementById("version")
+        ver.innerText = "v"+json["version"]
+    });
 }
 
 function closeSettings(){
-    $(".settings").css("right", "-360px")
+    $(".settings").css("right", "-800px")
 }
+
+$('#range-rows').on('input', function (e){
+    let rows = e.target.value
+    document.getElementById("rows").innerText = rows
+})
+
+$('#range-cols').on('input', function (e){
+    let cols = e.target.value
+    document.getElementById("cols").innerText = cols
+})
