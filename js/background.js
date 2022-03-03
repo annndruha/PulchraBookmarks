1bbb47ec5ed0f2e040e8ab99828feae51483718f
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-03-03 21:17:34 +0300

    Save and load cloud

--- js/background.js
+++ js/background.js
@@ -1,9 +1,23 @@
 // noinspection JSDeprecatedSymbols
 
+function varDefined(link) {
+    return link !== 'undefined' && link !== 'null' && link !== null && link !== undefined && link !== ''
+}
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed')
     chrome.tabs.create({'url': 'chrome://newtab/'})
-    const json = JSON.parse("{\"10\":{\"0\":{\"link\":\"https://drive.google.com/drive/u/0/my-drive\"}},\"11\":{\"0\":{\"link\":\"https://twitter.com/\"}},\"12\":{\"0\":{\"link\":\"https://youtube.com/\"}},\"13\":{\"0\":{\"link\":\"https://www.instagram.com/\"}},\"14\":{\"0\":{\"link\":\"https://www.spotify.com/\"}},\"15\":{\"0\":{\"link\":\"https://mail.yandex.ru/touch\"}},\"20\":{\"0\":{\"link\":\"https://www.facebook.com/\"}},\"21\":{\"0\":{\"link\":\"https://www.amazon.com/\"}},\"22\":{\"0\":{\"link\":\"https://www.pinterest.com/\"}},\"23\":{\"0\":{\"link\":\"https://www.imdb.com/\"}},\"24\":{\"0\":{\"link\":\"https://github.com/Annndruha/pulchra-bookmarks\"}},\"25\":{\"0\":{\"link\":\"mail.ximc.ru\"}},\"00\":{\"0\":{\"link\":\"http://www.google.com/\"}},\"01\":{\"0\":{\"link\":\"https://wikipedia.org/\"}},\"02\":{\"0\":{\"link\":\"https://translate.google.com/\"}},\"03\":{\"0\":{\"link\":\"https://netflix.com\"}},\"04\":{\"0\":{\"link\":\"https://www.reddit.com/\"}},\"05\":{\"0\":{\"link\":\"https://mail.google.com/mail\"}},\"cols\":5,\"new-tab\":false,\"rows\":3,\"show-quick\":true,\"version\":\"0.4.0\"}")
-    chrome.storage.local.clear()
-    chrome.storage.local.set(json, () => {})
+    chrome.storage.sync.get(null, (res)=> {
+        if (varDefined(res['rows'])){
+            console.log('Load start bookmarks from cloud')
+            chrome.storage.local.clear()
+            chrome.storage.local.set(res, () => {})
+            initSettingsValues(true)
+        }
+        else {
+            const json = JSON.parse("{\"10\":{\"0\":{\"link\":\"https://drive.google.com/drive/u/0/my-drive\"}},\"11\":{\"0\":{\"link\":\"https://twitter.com/\"}},\"12\":{\"0\":{\"link\":\"https://youtube.com/\"}},\"13\":{\"0\":{\"link\":\"https://www.instagram.com/\"}},\"14\":{\"0\":{\"link\":\"https://www.spotify.com/\"}},\"15\":{\"0\":{\"link\":\"https://mail.yandex.ru/touch\"}},\"20\":{\"0\":{\"link\":\"https://www.facebook.com/\"}},\"21\":{\"0\":{\"link\":\"https://www.amazon.com/\"}},\"22\":{\"0\":{\"link\":\"https://www.pinterest.com/\"}},\"23\":{\"0\":{\"link\":\"https://www.imdb.com/\"}},\"24\":{\"0\":{\"link\":\"https://github.com/Annndruha/pulchra-bookmarks\"}},\"25\":{\"0\":{\"link\":\"mail.ximc.ru\"}},\"00\":{\"0\":{\"link\":\"http://www.google.com/\"}},\"01\":{\"0\":{\"link\":\"https://wikipedia.org/\"}},\"02\":{\"0\":{\"link\":\"https://translate.google.com/\"}},\"03\":{\"0\":{\"link\":\"https://netflix.com\"}},\"04\":{\"0\":{\"link\":\"https://www.reddit.com/\"}},\"05\":{\"0\":{\"link\":\"https://mail.google.com/mail\"}},\"cols\":5,\"new-tab\":false,\"rows\":3,\"show-quick\":true,\"version\":\"0.4.0\"}")
+            chrome.storage.local.clear()
+            chrome.storage.local.set(json, () => {})
+        }
+    })
 })
--- js/saveopen.js
+++ js/saveopen.js
@@ -11,6 +11,7 @@ function saveToFile() {
         }).appendTo('body').click(() => {
             $(this).remove()
         })[0].click()
+        console.log('Save bookmarks to file')
     })
 }
 
@@ -23,10 +24,47 @@ function loadFromFile() {
         chrome.storage.local.clear()
         chrome.storage.local.set(json, () => {})
         initSettingsValues(true)
+        console.log('Load bookmarks from file')
     }
     } catch (e) {
         if (e instanceof TypeError) {} else {console.log(e)}
     }
+}
+
+function saveToCloud() {
+    chrome.storage.local.get(null, (res) => {
+        chrome.storage.sync.set(res, () => {
+            console.log('Save bookmarks in cloud')
+            let save_icon = document.getElementById('icon-cloud-save')
+            save_icon.setAttribute("src","images/icons/cloud_done.svg")
+            setTimeout(function() {
+                let load_icon = document.getElementById('icon-cloud-save')
+                load_icon.setAttribute("src","images/icons/backup.svg")
+            }, 1500);
+        })
+    })
+}
+
+function loadFromCloud() {
+    chrome.storage.sync.get(null, (res) => {
+        if (varDefined(res['rows'])){
+            console.log('Load bookmarks from cloud')
+            chrome.storage.local.clear()
+            chrome.storage.local.set(res, () => {})
+            initSettingsValues(true)
+
+            let load_icon = document.getElementById('icon-cloud-load')
+            load_icon.setAttribute("src","images/icons/cloud_done.svg")
+            setTimeout(function() {
+                let load_icon = document.getElementById('icon-cloud-load')
+                load_icon.setAttribute("src","images/icons/cloud_download.svg")
+            }, 1500);
+        }
+        else {
+            alert("Cloud hasn't any data")
+        }
+
+    })
 }
 
 $('#save-to-file').on('click', function (e) {
@@ -37,4 +75,14 @@ $('#save-to-file').on('click', function (e) {
 $('#upload_input').on('change', function (e) {
     e.stopPropagation()
     loadFromFile()
+})
+
+$('#save-to-cloud').on('click', function (e) {
+    e.stopPropagation()
+    saveToCloud()
+})
+
+$('#load-from-cloud').on('click', function (e) {
+    e.stopPropagation()
+    loadFromCloud()
 })
