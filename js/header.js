e58e6546d443c563003f468c8aab527ab4f06b9b
Andrey Marakulin <a.p.marakulin@gmail.com>
2022-03-08 20:36:42 +0300

    Owerflow

--- js/header.js
+++ js/header.js
@@ -1,28 +1,32 @@
 function createBookmarks() {
     chrome.bookmarks.getTree((bookmarkTreeNodes) => {
         let root = bookmarkTreeNodes[0]["children"][0]["children"]
-        console.log(root)
         for (let i=0; i < root.length; i++){
             let root_str = '<span class="header-item" id="root-header-' + root[i].id + '">'
             let root_item = $(root_str)
             $('#bookmarks').append(root_item.text(root[i].title))
             $('#root-header-'+root[i].id).on('click', createRootElementTree)
         }
-        // dumpTreeNodes(bookmarkTreeNodes[0]["children"][0]["children"])
-        //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes[0]["children"][0]["children"]))
     })
 }
 
 function createRootElementTree(){
     let root_id = this.id.replace('root-header-', '')
     chrome.bookmarks.getSubTree(root_id, (rootitemNodes) => {
-        console.log(rootitemNodes[0])
+        if (rootitemNodes[0].children){
+        let root_popup = document.createElement('div')
+        let app_container = document.getElementById('app-container')
+        root_popup.id = "root_popup"
+        app_container.appendChild(root_popup).className = 'root_popup'
+        $(root_popup).append(dumpTreeNodes(rootitemNodes[0]['children']))}
+        else{
+            chrome.tabs.create({ url: rootitemNodes[0].url })
+        }
     })
-    // $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes[0]["children"][0]["children"]))
 }
 
 function dumpTreeNodes(bookmarkNodes) {
-    let list = $('<ul hidden>')
+    let list = $('<ul>')
     for (let i = 0; i < bookmarkNodes.length; i++) { //bookmarkNodes.length
         list.append(dumpNode(bookmarkNodes[i]))
     }
