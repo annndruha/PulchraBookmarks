aa0e6a9e852adea62dbdaee870d005755adc766e
Andrey Marakulin <a.p.marakulin@gmail.com>
2022-03-09 23:00:31 +0300

    Fix few bugs

--- js/saveopen.js
+++ js/saveopen.js
@@ -17,17 +17,25 @@ function saveToFile() {
 
 function loadFromFile() {
     try {
-    let fileReader = new FileReader()
-    fileReader.readAsDataURL($('#upload_input').prop('files')[0])
-    fileReader.onload = () => {
-        const json = JSON.parse(atob(fileReader.result.substring(29)))
-        chrome.storage.local.clear()
-        chrome.storage.local.set(json, () => {})
-        initSettingsValues(true)
-        console.log('Load bookmarks from file')
-    }
+        let fileReader = new FileReader()
+        fileReader.readAsDataURL($('#upload_input').prop('files')[0])
+        fileReader.onload = () => {
+            try {
+                const json = JSON.parse(atob(fileReader.result.substring(29)))
+                chrome.storage.local.clear()
+                chrome.storage.local.set(json, () => {
+                })
+                initSettingsValues(true)
+                console.log('Load bookmarks from file')
+            } catch (e) {
+                alert('Broken file!\n' + e.toString())
+            }
+        }
     } catch (e) {
-        if (e instanceof TypeError) {} else {console.log(e)}
+        if (e instanceof TypeError) {
+        } else {
+            console.log(e)
+        }
     }
 }
 
@@ -36,10 +44,10 @@ function saveToCloud() {
         chrome.storage.sync.set(res, () => {
             console.log('Save bookmarks in cloud')
             let save_icon = document.getElementById('icon-cloud-save')
-            save_icon.setAttribute("src","images/icons/cloud_done.svg")
-            setTimeout(function() {
+            save_icon.setAttribute("src", "images/icons/cloud_done.svg")
+            setTimeout(function () {
                 let load_icon = document.getElementById('icon-cloud-save')
-                load_icon.setAttribute("src","images/icons/backup.svg")
+                load_icon.setAttribute("src", "images/icons/backup.svg")
             }, 1500);
         })
     })
@@ -47,23 +55,22 @@ function saveToCloud() {
 
 function loadFromCloud() {
     chrome.storage.sync.get(null, (res) => {
-        if (varDefined(res['rows'])){
+        if (varDefined(res['rows'])) {
             console.log('Load bookmarks from cloud')
             chrome.storage.local.clear()
-            chrome.storage.local.set(res, () => {})
+            chrome.storage.local.set(res, () => {
+            })
             initSettingsValues(true)
 
             let load_icon = document.getElementById('icon-cloud-load')
-            load_icon.setAttribute("src","images/icons/cloud_done.svg")
-            setTimeout(function() {
+            load_icon.setAttribute("src", "images/icons/cloud_done.svg")
+            setTimeout(function () {
                 let load_icon = document.getElementById('icon-cloud-load')
-                load_icon.setAttribute("src","images/icons/cloud_download.svg")
+                load_icon.setAttribute("src", "images/icons/cloud_download.svg")
             }, 1500);
-        }
-        else {
+        } else {
             alert("Cloud hasn't any data")
         }
-
     })
 }
 
