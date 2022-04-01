cad9e1725836c501bdf518f9bcb0c3b4dca507a6
Andrey Marakulin <a.p.marakulin@gmail.com>
2022-04-01 20:57:05 +0300

    Add dark theme, separate to stylesheets

--- slider.js
+++ slider.js
@@ -4,3 +4,19 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
     e.style.setProperty('--max', e.max === '' ? '100' : e.max);
     e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+$('#theme').on('change', function () {
+    let theme = $(this).val()
+    if(theme === 'Windows 11 Light') {
+        // $('body').css('background-color', '#000')
+        document.getElementById('colorsheet').href = 'css/colorsheet_w11_light.css'
+    }
+    if (theme === 'Windows 11 Dark'){
+        // $('body').css('background-color', '#fff')
+        document.getElementById('colorsheet').href = 'css/colorsheet_w11_dark.css'
+    }
+})
