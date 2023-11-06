8c822521154c8993b2bb8c2d8b2d31cb5549acf9
Andrey Marakulin <annndruha.github@gmail.com>
2023-11-06 17:32:22 +0300

    1.0.12 Load cloud save when install

--- js/background.js
+++ js/background.js
@@ -2,6 +2,23 @@
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed')
-    chrome.tabs.create({'url': 'chrome://newtab/'})
-    chrome.storage.local.clear()
+
+    function varDefined(v) {
+        return v !== 'undefined' && v !== 'null' && v !== null && v !== undefined && v !== ''
+    }
+
+    chrome.storage.sync.get(null, (json) => {
+        if (varDefined(json['rows'])) {
+            console.log('Load bookmarks from cloud...')
+            chrome.storage.local.clear(() => {
+                chrome.storage.local.set(json, () => {
+                    console.log('Cloud bookmarks set to local storage.')
+                    chrome.tabs.create({'url': 'chrome://newtab/'})
+                })
+            })
+        } else {
+            console.log('Cloud hasn\'t any data')
+            chrome.tabs.create({'url': 'chrome://newtab/'})
+        }
+    })
 })
