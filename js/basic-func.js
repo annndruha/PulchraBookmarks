0cda12017d17b0e9a83e1397b818a5ba18c2ccc6
Andrey Marakulin <a.p.marakulin@gmail.com>
2024-08-07 00:51:16 +0300

    Update basic-func.js

--- js/basic-func.js
+++ js/basic-func.js
@@ -35,24 +35,14 @@ function timeSince(date) {
     let seconds = Math.floor((new Date() - date) / 1000);
     let interval = seconds / 31536000;
 
-    if (interval >= 1) {
-        return Math.floor(interval) + " years ago";
-    }
+    if (interval >= 1) { return Math.floor(interval) + " years ago";}
     interval = seconds / 2592000;
-    if (interval >= 1) {
-        return Math.floor(interval) + " months ago";
-    }
+    if (interval >= 1) {return Math.floor(interval) + " months ago";}
     interval = seconds / 86400;
-    if (interval >= 1) {
-        return Math.floor(interval) + " days ago";
-    }
+    if (interval >= 1) {return Math.floor(interval) + " days ago";}
     interval = seconds / 3600;
-    if (interval >= 1) {
-        return Math.floor(interval) + " hours ago";
-    }
+    if (interval >= 1) {return Math.floor(interval) + " hours ago";}
     interval = seconds / 60;
-    if (interval >= 1) {
-        return Math.floor(interval) + " minutes ago";
-    }
+    if (interval >= 1) {return Math.floor(interval) + " minutes ago";}
     return Math.floor(seconds) + " seconds ago";
 }
